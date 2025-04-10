fc13b5f833b7f11e13a64b20717058c62233b12a
alexgord0n <157073280+alexgord0n@users.noreply.github.com>
2025-04-10 16:04:21 -0700

    Update alexgordon-custom.js

--- alexgordon/alexgordon-custom.js
+++ alexgordon/alexgordon-custom.js
@@ -1,11 +1,8 @@
 // Hide Events for employees
-
-<script>
-  if (inSidedData.user.role.includes('Employee')) { 
-    document.querySelectorAll('li.header-navigation_list-item a').forEach(link => {
-      if (link.classList.contains('title-events')) {
-        link.closest('li').style.display = 'none';
-      }
-    });
-  }
-</script>
+if (inSidedData.user.role.includes('Employee')) { 
+  document.querySelectorAll('li.header-navigation_list-item a').forEach(link => {
+    if (link.classList.contains('title-events')) {
+      link.closest('li').style.display = 'none';
+    }
+  });
+}
