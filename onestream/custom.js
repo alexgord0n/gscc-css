65461ffde0c15ec52bfb034be0c54f797495f228
alexgord0n <157073280+alexgord0n@users.noreply.github.com>
2025-09-09 05:10:59 -0700

    Update custom.js

--- onestream/custom.js
+++ onestream/custom.js
@@ -1,11 +1,13 @@
-// Hide Menu Items
+if (inSidedData.user.role.includes('roles.guest')) {
+  const hideClasses = ['title-ideastream', 'title-partners', 'title-internal'];
 
-  if (inSidedData.user.role.includes('roles.guest')) {
-    const hideClasses = ['title-ideastream', 'title-partners', 'title-internal'];
-
-    document.querySelectorAll('li.header-navigation_list-item a').forEach(link => {
-      if (hideClasses.some(cls => link.classList.contains(cls))) {
-        link.closest('li').style.display = 'none';
-      }
-    });
-  }
+  // Hide Menu Items
+  document.querySelectorAll('li.header-navigation_list-item a').forEach(link => {
+    if (hideClasses.some(cls => link.classList.contains(cls))) {
+      link.closest('li').style.display = 'none';
+    }
+  });
+  
+  // Hide the article.quicklink-icon containing an <a> with href="https://procore-en-community.insided.com/ideas"
+  document.querySelector('article.quicklink-icon a[href="https://gordonhockey-en-community.insided.com/ideas"]')?.closest('article.quicklink-icon')?.style.setProperty("display", "none");
+}
