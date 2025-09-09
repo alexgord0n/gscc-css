92ada9cdbc33d5f9d2758ca7be0779f1714d5f08
alexgord0n <157073280+alexgord0n@users.noreply.github.com>
2025-09-09 04:57:42 -0700

    Update custom.js

--- onestream/custom.js
+++ onestream/custom.js
@@ -1,10 +1,12 @@
 /* Hide Menu Items */
 <script>
   if (inSidedData.user.role.includes('roles.guest')) {
-    const hideClasses = ['title-ideastream', 'title-partners', 'title-Internal'];
-
     document.querySelectorAll('li.header-navigation_list-item a').forEach(link => {
-      if (hideClasses.some(cls => link.classList.contains(cls))) {
+      if (
+        link.classList.contains('title-ideastream') ||
+        link.classList.contains('title-partners') ||
+        link.classList.contains('title-internal')
+      ) {
         link.closest('li').style.display = 'none';
       }
     });
